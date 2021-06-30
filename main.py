8babd6ddac36beddf957664000e6cd308a49676a
Matthew Whyte <matthewwhyte14@gmail.com>
2021-06-30 09:03:30 +0100

    Added new code for weighting

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import random
 
 
 def angle_dist(a, b):
+    a = np.rad2deg(a)
+    b = np.rad2deg(b)
     clockwise_angle = np.abs(b-a)
-    anticlockwise_angle = np.abs( min(a,b) + (np.pi*2 - max(a,b)) )
+    anticlockwise_angle = np.abs( min(a,b) + (360.0 - max(a,b)) )
     return min(clockwise_angle,anticlockwise_angle)
 
 
@@ -62,7 +65,8 @@ def save_weights(data, p=np.linspace(1,4,21)):
 
 
 data = [np.deg2rad(x) for x in json.load(open("az_data.txt"))["val"]]
-save_weights(data, np.linspace(4,8,21))
+
+save_weights(data, np.array([2.0]))
 
 '''
 data = [np.deg2rad(x) for x in json.load(open("az_data.txt"))["val"]]
@@ -76,15 +80,48 @@ plt.plot(powr, err_vals/4.0)
 plt.savefig("total-best-power.png")'''
 
 
-'''
-scores = np.array([1 if d <= np.pi/4 else 0.0 for d in data])
-print("Unweighted")
-print(np.sum(scores)/len(data))
+def get_bins(weights, data):
+    weighted_bins = []
+    for i in range(24):
+        s = sum([weights[j] if np.deg2rad(15 * i) <= d < np.deg2rad(15 * (i + 1)) else 0.0 for j, d in enumerate(data)])
+        weighted_bins.append(s)
+    return weighted_bins
 
-weights = np.load(open("weights/weight-val-4.0.npy", "rb"))
-print("Weighted")
-print(np.dot(weights,scores))'''
 
+w = np.load(open("weights/weight-val-2.0.npy", "rb"))
+bins = get_bins(w, data)
+
+f = plt.figure()
+ax = f.add_subplot(projection="polar")
+new_bins = get_bins([1.0/len(data) for _ in range(len(data))], data)
+ax.plot([np.pi*2/24.0*i+np.pi/24.0 for i in range(24)] + [np.pi/24.0], new_bins + [new_bins[0]])
+ax.plot([np.pi*2/24.0*i+np.pi/24.0 for i in range(24)] + [np.pi/24.0], bins + [bins[0]])
+plt.savefig("test.png")
+
+sys.exit()
+
+min_b = min(bins)
+max_b = max(bins)
+
+plt.plot([i*np.pi/12+np.pi/24 for i in range(0,24)], bins)
+
+ct = 0
+
+for i in range(24):
+    print("Slice {}".format(i))
+    while bins[i] > 1.1*min_b:
+        idx = []
+        for j,d in enumerate(data):
+            if np.deg2rad(15 * i) <= d < np.deg2rad(15 * (i + 1)):
+                idx.append(j)
+        #r_idx = idx[random.randint(0, len(idx))]
+        w[idx] = w[idx]*0.95
+        bins = get_bins(w, data)
+
+plt.plot([i*np.pi/12+np.pi/24 for i in range(0,24)], bins)
+plt.show()
+
+np.save(open("weights/pascal3d-val-weights.npy", "wb"), w)
 
 '''
 data = [np.deg2rad(x) for x in json.load(open("az_data.txt"))["val"][:4000]]
