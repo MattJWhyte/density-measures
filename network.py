6d86950cb49ce2d1e0c3daa828664f47f39a936c
Matthew Whyte <matthewwhyte14@gmail.com>
2021-07-05 17:55:05 +0100

    Try simple net

--- network.py
+++ network.py
@@ -27,7 +27,7 @@ class ValNet(nn.Module):
 
     def __init__(self, n=11206):
         super(ValNet, self).__init__()
-        # No more than 1024
+
         self.fc1 = nn.Linear(n, 360)
         self.fc2 = nn.Linear(360, 1)
 
@@ -48,11 +48,10 @@ class ValNetSimp(nn.Module):
 
     def __init__(self, n=11206):
         super(ValNetSimp, self).__init__()
-        # No more than 1024
         self.fc1 = nn.Linear(n, 1)
 
     def forward(self, x):
-        return F.relu(self.fc1(x))
+        return F.sigmoid(self.fc1(x))
 
     def save(self, PATH):
         torch.save(self.state_dict(), PATH)
